86f9538bc56ceac4ebadd58bba3070f50a4c93d0
Mpanuga <Manikanta_panuganti@gap.com>
2021-05-16 22:53:55 +0530

    Adds GET endpoint to get a user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const User = require('../models/user')
+const crypto = require("crypto");
+
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-
+    const id = crypto.randomBytes(16).toString("hex");
     let user = new User({
+        id:id,
         name: req.body.name,
         dob: req.body.dob,
         descriptoin: req.body.descriptoin,
@@ -13,6 +16,12 @@ router.post('/', async (req, res) => {
     user = await user.save()
     return res.send(user)
 
+})
+
+
+router.get('/:id', async (req, res) => {
+    const user = await User.find({id:req.params.id})
+    return res.send(user)
 })
 
 module.exports = router 
